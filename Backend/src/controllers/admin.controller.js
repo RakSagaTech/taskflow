5a62d01ab04bacbd8a691bcdd3f290711fcd8f27
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-07-20 23:50:18 +0530

    feat: implement admin controller to fetch a user by ID

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -4,11 +4,43 @@ export const getAllUsersController = async (req, res) => {
   try {
 
     const users = User.find({ role: "user" }).select("-password");
-    
+
     return res.status(200).json({
       message: "Users fetched successfully",
       data: {
         users,
+      },
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+
+  }
+};
+
+
+export const getUserController = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+    const user = await User.findById(id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "User fetched successfully",
+      data: {
+        user,
       },
     });
 
